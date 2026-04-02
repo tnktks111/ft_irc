037946a0cb42b9d024f16934db67da175c8c8c76
tnktks111 <tnk-tsks17074@g.ecc.u-tokyo.ac.jp>
2026-04-02 15:33:03 +0900

    refac: clientにuserとrealを追加,client側にvalidationを持たせる

--- include/domain/Client.hpp
+++ include/domain/Client.hpp
@@ -4,15 +4,17 @@
 #include <string>
 
 class Client {
-private:
+ private:
   Client();
-  Client(const Client &other);
-  Client &operator=(const Client &other);
+  Client(const Client& other);
+  Client& operator=(const Client& other);
 
   int _fd;
   const std::string _host;
   std::string _nickName;
   std::string _userName;
+  std::string _realName;
+  unsigned int _mode;
 
   std::string _recvBuffer;
   std::string _sendBuffer;
@@ -20,28 +22,38 @@ private:
   bool _isPassChecked;
   bool _isRegistered;
 
-public:
+  // validation helper
+  static bool _isLetter(char c);
+  static bool _isDigit(char c);
+  static bool _isSpecial(char c);
+  static bool _isProhibittedCharInUserName(char c);
+
+ public:
   Client(int fd, const std::string& host);
   ~Client();
 
+  // validator
+  static bool isValidNickName(const std::string& token);
+  static bool isValidUserName(const std::string& token);
+
   int getFd() const;
-  void appendRecvBuffer(const std::string &data);
+  void appendRecvBuffer(const std::string& data);
   std::string extractMessage();
 
   // sendBuffer
-  void appendSendBuffer(const std::string &msg);
-  std::string &getSendBuffer();
+  void appendSendBuffer(const std::string& msg);
+  std::string& getSendBuffer();
   void eraseSendBuffer(size_t length);
 
   // Getter
-  const std::string &getNickName() const;
-  const std::string &getUserName() const;
+  const std::string& getNickName() const;
+  const std::string& getUserName() const;
   bool isPassChecked() const;
   bool isRegistered() const;
 
   // Setter
-  void setNickName(const std::string &nickName);
-  void setUserName(const std::string &userName);
+  void setNickName(const std::string& nickName);
+  void setUserName(const std::string& userName);
   void setPassChecked(bool status);
   void setRegistered(bool status);
 
--- src/domain/Client.cpp
+++ src/domain/Client.cpp
@@ -1,15 +1,66 @@
 #include "Client.hpp"
 #include <unistd.h>
 
-Client::Client(int fd, const std::string &host)
-    : _fd(fd), _host(host), _nickName(""), _userName(""), _recvBuffer(""),
-      _sendBuffer(""), _isPassChecked(false), _isRegistered(false) {}
+Client::Client(int fd, const std::string& host)
+    : _fd(fd),
+      _host(host),
+      _nickName(""),
+      _userName(""),
+      _recvBuffer(""),
+      _sendBuffer(""),
+      _isPassChecked(false),
+      _isRegistered(false) {}
 
-Client::~Client() { close(_fd); }
+Client::~Client() {
+  close(_fd);
+}
 
-int Client::getFd() const { return _fd; }
+// validation helper
+bool Client::_isLetter(char c) {
+  return ((0x41 <= c && c <= 0x5a) || (0x61 <= c && c <= 0x7a));
+}
 
-void Client::appendRecvBuffer(const std::string &data) { _recvBuffer += data; }
+bool Client::_isDigit(char c) {
+  return (0x30 <= c && c <= 0x39);
+}
+
+bool Client::_isSpecial(char c) {
+  return ((0x5b <= c && c <= 0x60) || (0x7b <= c && c <= 0x7d));
+}
+
+bool Client::isValidNickName(const std::string& token) {
+  if (token.length() > 9 || token.empty())
+    return false;
+  if (_isLetter(*token.begin()) == false && _isSpecial(*token.begin()) == false)
+    return false;
+  for (std::string::const_iterator it = token.begin() + 1; it != token.end();
+       ++it) {
+    if (!_isLetter(*it) && !_isDigit(*it) && !_isSpecial(*it) && *it != '-')
+      return false;
+  }
+  return true;
+}
+
+bool Client::_isProhibittedCharInUserName(char c) {
+  return (c == 0x00 || c == 0x0D || c == 0x0A || c == ' ' || c == '@');
+}
+
+bool Client::isValidUserName(const std::string& token) {
+  for (std::string::const_iterator it = token.begin(); it != token.end();
+       ++it) {
+    if (_isProhibittedCharInUserName(*it))
+      return false;
+  }
+  return true;
+}
+
+int Client::getFd() const {
+  return _fd;
+}
+
+void Client::appendRecvBuffer(const std::string& data) {
+  _recvBuffer += data;
+}
 
 std::string Client::extractMessage() {
   // macのncは\nしかいれてくれないので仮で
@@ -30,21 +81,43 @@ std::string Client::extractMessage() {
 }
 
 // sendBuffer
-void Client::appendSendBuffer(const std::string &msg) { _sendBuffer += msg; }
-std::string &Client::getSendBuffer() { return _sendBuffer; }
-void Client::eraseSendBuffer(size_t length) { _sendBuffer.erase(0, length); }
+void Client::appendSendBuffer(const std::string& msg) {
+  _sendBuffer += msg;
+}
+std::string& Client::getSendBuffer() {
+  return _sendBuffer;
+}
+void Client::eraseSendBuffer(size_t length) {
+  _sendBuffer.erase(0, length);
+}
 
 // Getter
-const std::string &Client::getNickName() const { return _nickName; }
-const std::string &Client::getUserName() const { return _userName; }
-bool Client::isPassChecked() const { return _isPassChecked; }
-bool Client::isRegistered() const { return _isRegistered; }
+const std::string& Client::getNickName() const {
+  return _nickName;
+}
+const std::string& Client::getUserName() const {
+  return _userName;
+}
+bool Client::isPassChecked() const {
+  return _isPassChecked;
+}
+bool Client::isRegistered() const {
+  return _isRegistered;
+}
 
 // Setter
-void Client::setNickName(const std::string &nickName) { _nickName = nickName; }
-void Client::setUserName(const std::string &userName) { _userName = userName; }
-void Client::setPassChecked(bool status) { _isPassChecked = status; }
-void Client::setRegistered(bool status) { _isRegistered = status; }
+void Client::setNickName(const std::string& nickName) {
+  _nickName = nickName;
+}
+void Client::setUserName(const std::string& userName) {
+  _userName = userName;
+}
+void Client::setPassChecked(bool status) {
+  _isPassChecked = status;
+}
+void Client::setRegistered(bool status) {
+  _isRegistered = status;
+}
 
 std::string Client::getPrefix() const {
   return _nickName + "!" + _userName + "@" + _host;
