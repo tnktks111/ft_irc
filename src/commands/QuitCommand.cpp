70008252e060bbab054589f894ad1ef47c32aa49
sabe-0524 <soichiro.0524@icloud.com>
2026-07-05 01:55:20 +0900

    QUIT通知をIRC形式に修正

--- src/commands/QuitCommand.cpp
+++ src/commands/QuitCommand.cpp
@@ -4,7 +4,10 @@ QuitCommand::QuitCommand(ServerContext &serverCtx) : _serverCtx(serverCtx) {}
 QuitCommand::~QuitCommand() {}
 
 bool QuitCommand::execute(CommandContext &ctx) {
-  std::string quitMsg = (ctx.params().empty()) ? "Client Quit" : ctx.params()[0];
+  std::string reason =
+      (ctx.params().empty()) ? "Client Quit" : ctx.params()[0];
+  std::string quitMsg = ":" + ctx.prefix() + " QUIT :" + reason;
+
   _serverCtx.removeClientFromAllChannels(ctx.client(), quitMsg);
   return false;
 }
